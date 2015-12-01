9dca022cfceefc007a7c2686f804e474f7d5cc05
Matthias Pfefferle <matthias@pfefferle.org>
2015-12-01 22:44:47 +0100

    version bump

--- open-search-document.php
+++ open-search-document.php
@@ -3,7 +3,7 @@
 Plugin Name: Open Search Document
 Plugin URI: http://wordpress.org/plugins/open-search-document/
 Description: Create an Open Search Document for your blog.
-Version: 2.1.1
+Version: 2.1.2
 Author: johnnoone, pfefferle
 Author URI: https://github.com/pfefferle/wordpress-open-search-document/
 License: GPLv2 or later
